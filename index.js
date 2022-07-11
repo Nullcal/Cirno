156ca8c991caa99d54eac2f7b538a0a9546363af
Glitch (diamond-harmless-dragon) <none>
2022-07-11 07:30:52 +0000

    Import of Nullcal/GakunenKikaku

---
+++ index.js
@@ -0,0 +1,399 @@
+"use strict";
+
+// モジュール
+const http = require("http");
+const express = require("express");
+const socketIO = require("socket.io");
+const moment = require("moment");
+const fs = require("fs");
+// パス指定用モジュール
+const path = require('path');
+
+// オブジェクト
+const app = express();
+const server = http.Server(app);
+const io = socketIO(server);
+
+// 定数
+const PORT = process.env.PORT || 3000;
+
+// ログイン状況
+let loginStatus = [];
+let loginId = [];
+
+let teamrank = [];
+let teamscore = {};
+
+let anslgt = 0;
+
+let correctTeams = 0;
+
+let correspo = {};
+
+// タイマー管理用
+let left;
+
+// 問題情報
+let gqObj;
+
+// csv読み込み
+let srccsv = fs.readFileSync("./data/teamdata.csv", "utf8");
+let teamdata = srccsv.split(/\r\n|\n/g);
+for (var i = 0; i < teamdata.length; i++) {
+  teamdata[i] = teamdata[i].split(/,/);
+}
+
+let newCsv = teamdata.slice();
+
+// 問題文読み込み
+let quest = JSON.parse(fs.readFileSync("./data/question.json", "utf8"))[0];
+console.log(`\x1b[42mREAD_FILE\x1b[49m : ${quest})`);
+
+// タイマー読み込み
+let adtimer = JSON.parse(fs.readFileSync("./data/timer.json", "utf8"))[0];
+console.log(`\x1b[42mREAD_FILE\x1b[49m : ${adtimer})`);
+
+// アドミンログイン情報読み込み
+let addata = JSON.parse(fs.readFileSync("./data/userdata.json", "utf8"))[0];
+console.log(`\x1b[42mREAD_FILE\x1b[49m : ${addata})`);
+
+// 接続時の処理
+io.on("connection", (socket) => {
+  console.log(`connection    : ${socket.id}`);
+
+  // セレクトボックスの中身を生成
+  io.to(socket.id).emit("emptySelect");
+  for (let i = 0; i < teamdata.length; i++) {
+    io.to(socket.id).emit("generateSelect", teamdata[i][0]);
+  }
+
+  // ログインリクエスト
+  socket.on("loginReq", function(team) {
+    // 既存のログインユーザーがいるか確認
+    if (loginStatus.indexOf(team) == -1) {
+      //
+      correspo[socket.id] = team;
+      // 配列整理
+      loginStatus.push(team);
+      loginId.push(socket.id);
+      // テームのスコア取得
+      let score = NaN;
+      for (let i = 0; i < teamdata.length; i++) {
+        if(team == teamdata[i][0]) {
+          score = teamdata[i][1];
+        }
+      }
+      teamscore[socket.id] = score;
+      io.to(socket.id).emit("loginSuccess", team, score);
+      console.log(`\x1b[42mLOGIN \x1b[49m : ${team}(${socket.id})`);
+      //
+      io.emit("resLoginStatus", loginStatus);
+    } else {
+      io.to(socket.id).emit("loginFailed", team);
+      console.log("LOGIN FAILED.");
+    }
+  });
+
+  // ログイン済みのみ向けio.emit()
+  function limIoEmit(postId, arg1, arg2, arg3, arg4) {
+    for (var i = 0; i < loginId.length; i++) {
+      io.to(loginId[i]).emit(postId, arg1, arg2, arg3, arg4);
+    }
+  }
+
+  // ゲーム開始
+  socket.on("startReq", function(qNum) {
+    anslgt = 0;
+    //
+    if (loginStatus.length > 0) {
+      console.log("Game start!");
+      limIoEmit("startView"); // GO!の画面
+      io.emit("unrestartable");
+      // 問題情報取得
+      let qObj = quest[`q${qNum}`];
+      gqObj = qObj;
+      let display = qObj.title;
+      let options = qObj.options;
+      //
+      let timer;
+      clearInterval(timer);
+      // 問題表示
+      setTimeout(function() {
+        // ポイント付与
+        for (var i = 0; i < Object.keys(teamscore).length; i++) {
+          teamscore[Object.keys(teamscore)[i]] = parseInt(teamscore[Object.keys(teamscore)[i]]) + 10;
+        }
+        //
+        limIoEmit("voteView", qNum, display, options, teamscore);
+        // タイマー設定
+        left = adtimer.default;
+        //
+        timer = setInterval(() =>{
+          left--;
+          let mLeft = ("00" + Math.floor(left/60)).slice(-2);
+          let sLeft = ("00" + left % 60).slice(-2);
+          limIoEmit("refTimer", mLeft, sLeft);
+          //
+          if(left <= 0){
+            clearInterval(timer);
+            limIoEmit("timeIsUp");
+          }
+        }, 1000);
+      }, 1000);
+      // 順位計算用配列用意
+      teamrank = [];
+      for (var i = 0; i < teamdata.length; i++) {
+        // 順位割り出し
+        teamrank.push([undefined, -1]);
+      }
+      correctTeams = 0;
+    } else {
+      io.emit("failedLaunch");
+    }
+  });
+
+  // 解答集計
+  socket.on("postAnswer", function(data) {
+    let teamName = loginStatus[loginId.indexOf(socket.id)];
+    let newData = [teamName, NaN];
+    let teamIdx = 0;
+    for (var i = 0; i < teamdata.length; i++) {
+      if (teamdata[i][0] == teamName) {
+        newData[1] = parseInt(teamdata[i][1], 10);
+        teamIdx = i;
+      }
+    }
+    //
+    if (data.amount > teamscore[socket.id]) {
+      data.amount = teamscore[socket.id];
+    }
+    // 例外処理
+    if (newData[1] == NaN) {
+      console.log(`\x1b[41mERROR\x1b[49m : Teamdata(${teamName}) is missing.`);
+    }
+    //
+    let isCorrect = false;
+    let curt = teamscore[socket.id];
+    let result = curt - data.amount;  // 掛け金差し引き
+    let magnif = 0;
+    let cOption = gqObj.options[gqObj.answer].name;
+    let cMagnif = gqObj.options[gqObj.answer].magnification;
+    //
+    if (data.option == gqObj.answer) {
+      // correct
+      magnif = gqObj.options[gqObj.answer].magnification;
+      //
+      isCorrect = true;
+      correctTeams++;
+    } else {
+      // incorrect
+      magnif = 0.0;
+    }
+    result += data.amount * magnif;
+    if (result < 0) {
+      result = 0;
+    }
+    teamrank[teamIdx] = [socket.id, result];
+    // csvに保存
+    newCsv[teamIdx][1] = result;
+    teamscore[socket.id] = result;
+    //
+    io.to(socket.id).emit("refreshSpan", isCorrect, curt, result, data.amount, magnif, cOption, cMagnif);
+    //
+    if (anslgt == 0) {
+      io.emit("resetList");
+    }
+    io.emit("refreshList", [correspo[socket.id], curt, result]);
+    // すべてのクライアントから収集完了
+    anslgt++;
+    if (anslgt == loginId.length) {
+      // 順位確定 - 久しぶりのバブルソート
+      for (var i = 0; i < teamrank.length; i++) {
+        for (var j = teamrank.length - 1; j > i; j--) {
+          if (parseInt(teamrank[j][1]) < parseInt(teamrank[j-1][1])) {
+            let tmp = teamrank[j];
+            teamrank[j] = teamrank[j-1];
+            teamrank[j-1] = tmp;
+          }
+        }
+      }
+      teamrank.reverse();
+      //
+      for (var i = 0; i < teamrank.length; i++) {
+        if (teamrank[i][0] !== undefined) {
+          io.to(teamrank[i][0]).emit("refreshRank", i+1, [correctTeams, loginStatus.length]);
+        }
+      }
+      // csv書き込み
+      newCsv = newCsv.join("\r\n");
+      fs.writeFileSync("./data/teamdata.csv", newCsv);
+      console.log(`\x1b[44mAD_CSV \x1b[49m : Updated teamdata.csv by ${socket.id}`);
+      //
+      srccsv = fs.readFileSync("./data/teamdata.csv", "utf8");
+      teamdata = srccsv.split(/\r\n|\n/g);
+      for (var i = 0; i < teamdata.length; i++) {
+        teamdata[i] = teamdata[i].split(/,/);
+      }
+      //
+      io.emit("referCsv", srccsv);
+      //
+      newCsv = teamdata.slice();
+      //
+      io.emit("showable");
+    }
+  });
+
+  // 結果発表へ遷移
+  socket.on("reqResultView", function() {
+    limIoEmit("resultView");
+  });
+
+  // ログアウト処理
+  function logout(id) {
+    // ログイン中のチームから削除
+    let teamIdx = loginId.indexOf(id);
+    console.log(`\x1b[43mLOGOUT\x1b[49m : ${loginStatus[teamIdx]}(${id})`);
+    if (teamIdx !== -1) {
+      loginStatus.splice(teamIdx, 1);
+      loginId.splice(teamIdx, 1);
+    }
+    //
+    io.emit("resLoginStatus", loginStatus);
+  }
+
+  // 切断時の処理
+  socket.on("disconnect", () => {
+    logout(socket.id);
+  });
+
+  // ログアウトリクエスト
+  socket.on("logoutReq", function() {
+    logout(socket.id);
+  });
+
+  // 以下アドミン
+
+  // ログインリクエスト
+  socket.on("loginReqAd", function(user, pass) {
+    if (addata[user]) {
+      if (addata[user].password == pass) {
+        console.log(`\x1b[42mAD_SUCCESS\x1b[49m : ${socket.id}`);
+          io.to(socket.id).emit("lgSccAd");
+      } else {
+        console.log(`\x1b[43mAD_FAILED \x1b[49m : ${socket.id}`);
+        io.to(socket.id).emit("lgFalAd");
+      }
+    } else {
+      console.log(`\x1b[43mAD_FAILED \x1b[49m : ${socket.id}`);
+      io.to(socket.id).emit("lgFalAd");
+    }
+  });
+
+  // 問題取得
+  socket.on("getQjsonAd", function() {
+    io.to(socket.id).emit("getQjsonAd", quest, adtimer, teamdata);
+  });
+
+  // 問題JSON更新
+  socket.on("saveQuestJson", function(data) {
+    fs.writeFileSync("./data/question.json", `[${JSON.stringify(data)}]`);
+    console.log(`\x1b[44mAD_JSON\x1b[49m : Updated question.json by ${socket.id}`);
+    // 問題文再読み込み
+    quest = JSON.parse(fs.readFileSync("./data/question.json", "utf8"))[0];
+    console.log(`\x1b[42mREAD_FILE\x1b[49m : ${quest})`);
+    //
+    io.emit("savescc");
+  });
+
+  // タイマーJSON更新
+  socket.on("saveTimerJson", function(data) {
+    fs.writeFileSync("./data/timer.json", `[${JSON.stringify(data)}]`);
+    console.log(`\x1b[44mAD_JSON\x1b[49m : Updated timer.json by ${socket.id}`);
+    // 問題文再読み込み
+    adtimer = JSON.parse(fs.readFileSync("./data/timer.json", "utf8"))[0];
+    console.log(`\x1b[42mREAD_FILE\x1b[49m : ${adtimer})`);
+    //
+    io.emit("savescc");
+  });
+
+  // ゲーム終了
+  socket.on("terminateReq", function() {
+    left = 0;
+  });
+
+  // csv取得
+  socket.on("referCsv", function() {
+    io.emit("referCsv", srccsv);
+  });
+
+  // csv更新
+  socket.on("refreshCvs", function(file) {
+    srccsv = file;
+    teamdata = srccsv.split(/\r\n|\n/g);
+    for (var i = 0; i < teamdata.length; i++) {
+      teamdata[i] = teamdata[i].split(/,/);
+    }
+    //
+    io.emit("referCsv", srccsv);
+    io.emit("referCsvCli");
+    //
+    newCsv = teamdata.slice();
+  });
+
+  // ログイン状況取得
+  socket.on("getLoginStatus", function() {
+    io.emit("resLoginStatus", loginStatus);
+  });
+
+  // 最終結果表示
+  socket.on("finrView", function() {
+    //
+    let data = {};
+    // チーム名と得点を得点順にソート
+    let ranking = teamdata.slice();
+    for (var i = 0; i < ranking.length; i++) {
+      for (var j = ranking.length - 1; j > i; j--) {
+        if (parseInt(ranking[j][1]) < parseInt(ranking[j-1][1])) {
+          let tmp = ranking[j];
+          ranking[j] = ranking[j-1];
+          ranking[j-1] = tmp;
+        }
+      }
+    }
+    ranking.reverse();
+    data.rank = ranking;
+    //
+    //
+    io.emit("finrView", data);
+  });
+
+  // 暫定順位更新
+  // 上からコピーしたので冗長です
+  socket.on("refreshPod", function() {
+    //
+    let data = {};
+    // チーム名と得点を得点順にソート
+    let ranking = teamdata.slice();
+    for (var i = 0; i < ranking.length; i++) {
+      for (var j = ranking.length - 1; j > i; j--) {
+        if (parseInt(ranking[j][1]) < parseInt(ranking[j-1][1])) {
+          let tmp = ranking[j];
+          ranking[j] = ranking[j-1];
+          ranking[j-1] = tmp;
+        }
+      }
+    }
+    ranking.reverse();
+    data.rank = ranking;
+    //
+    io.to(socket.id).emit("refreshPod", data);
+  });
+});
+
+// 公開フォルダの指定
+app.use(express.static(path.join(__dirname, "public")));
+
+// サーバーの起動
+server.listen(PORT, () => {
+  console.log("server starts on port: %d", PORT);
+});
---
+++ public/client.js
@@ -0,0 +1,238 @@
+"use strict";
+
+// クライアントからサーバーへの接続要求
+const socket = io.connect();
+
+let loadpast = true;
+
+// デバイスのチーム
+let ownteam = null;
+let ownscore = undefined;
+
+// 接続時の処理
+socket.on("connect", () => {
+  for (var i = 0; i < 32; i++) {
+    console.log(
+      "%c　警告　%c\n ここにコードを書かないでください。最悪の場合ゲームが進行不能になります。",
+      "color:white; background-color:red; padding:2px; border-radius:4px; font-size: 32px; font-weight: 900;",
+      "font-size: 24px; font-weight: 900;"
+    );
+    console.info("");
+  }
+});
+
+$(function() {
+  // セレクトボックスの中身を生成
+  socket.on("emptySelect", function(){
+    $("#teamId").empty();
+  });
+  socket.on("generateSelect", function(rowData){
+    $("#teamId").append(`<option value="${rowData}">${rowData}</option>`);
+  });
+
+  // ログインリクエスト
+  $("#login").on("click", function() {
+    socket.emit("loginReq", $("#teamId").val());
+  });
+
+  // トランジション
+  function transMove(eFrom, eTo, dir) {
+    let asset = ["move", "back"];
+    $(".transitionPanels").css("display", "block");
+    //
+    $(".dto").removeClass(`${asset[1-dir]}to`);
+    $(".dto").removeClass(`${asset[1-dir]}from`);
+    //
+    $(".dto").removeClass(`${asset[dir]}to`);
+    $(".dfrom").addClass(`${asset[dir]}from`);
+    //
+    setTimeout(function() {
+      $(".display").css("display", "none");
+      eFrom.css("display", "none");
+      eTo.css("display", "flex");
+      //
+      $(".dfrom").removeClass(`${asset[dir]}from`);
+      $(".dto").addClass(`${asset[dir]}to`);
+      setTimeout(function() {
+        $(".transitionPanels").css("display", "none");
+      }, 400);
+    }, 400);
+  }
+
+  // ログイン成功
+  socket.on("loginSuccess", function(team, score) {
+    // ログインしたチームを記録
+    ownteam = team;
+    ownscore = score;
+    //console.log(ownteam, ownscore);
+    //
+    $(".sinfo").text(`チーム${ownteam}のポイントは...`);
+    $(".wscore").text(`${ownscore}P`);
+    //
+    transMove($(".welcome"), $(".waitroom"), 0);
+  });
+
+  // エラー表示
+  function showErrorPopup() {
+    $(".loginFailed").css("display", "block");
+    $(".loginFailed").removeClass("bgblurhide");
+    $(".loginFailed").addClass("bgblursnow");
+    $(".popup").css("display", "none");
+    setTimeout(function() {
+      $(".popup").css("display", "flex");
+      $(".popup").removeClass("popupscalehide");
+      $(".popup").addClass("popupscaleshow");
+    }, 200);
+    setTimeout(function() {
+      $(".loginFailed").addClass("bgblur");
+    }, 400);
+  }
+
+  // ログイン失敗
+  socket.on("loginFailed", function(team) {
+    // ログイン失敗ポップアップ
+    showErrorPopup();
+  });
+  // ポップアップ閉じる
+  $(".closepopup").on("click", function() {
+    $(".popup").removeClass("popupscaleshow");
+    $(".popup").addClass("popupscalehide");
+    $(".loginFailed").removeClass("bgblursnow");
+    $(".loginFailed").addClass("bgblurhide");
+    setTimeout(function() {
+      $(".popup").css("display", "none");
+    }, 200);
+    setTimeout(function() {
+      $(".loginFailed").css("display", "none");
+      $(".loginFailed").removeClass("bgblur");
+    }, 400);
+  });
+
+  // ログアウトリクエスト
+  $("#logout").on("click", function() {
+    socket.emit("logoutReq");
+    //
+    transMove($(".waitroom"), $(".welcome"), 1);
+  });
+
+  // ゲーム開始表示
+  socket.on("startView", function() {
+    transMove($(".waitroom"), $(".startview"), 0);
+  });
+
+  // ゲーム開始（問題表示）
+  socket.on("voteView", function(qNum, display, options, teamscore) {
+    // 毎ターンのポイント付与
+    ownscore = teamscore[socket.id];
+    //
+    $(".tQnum").text(`-Q${qNum}-`);
+    $(".tQuest").text(display);
+    $(".optContainer").empty();
+    for (var i = 0; i < options.length; i++) {
+      let optname = options[i].name;
+      let optval = options[i].id;
+      let optmag = options[i].magnification;
+      $(".optContainer").append(`<input type="radio" name="option" value="${optval}" id="opt${optval}"><label for="opt${optval}" class="optionLabel"><span class="tOptName">${optname}</span><span class="tOptMag">x${optmag}</span></label>`);
+      if (i == 0) {
+        $(`#opt${optval}`).attr("checked", true);
+      }
+    }
+    $("#rBetVal").attr("max", ownscore);
+    $(".tBetVal").text(`${$("#rBetVal").val()}P`);
+    $(".tScoreVal").text(`${ownscore}P`);
+    //
+    transMove($(".startview"), $(".voteview"), 0);
+  });
+
+  // レンジ内容反映
+  $("#rBetVal").on("input", function() {
+    $(".tBetVal").text(`${$("#rBetVal").val()}P`);
+  });
+
+  // タイマー更新
+  socket.on("refTimer", function(mLeft, sLeft) {
+    $(".tTimerVal").text(`${mLeft}:${sLeft}`);
+  });
+
+  // ゲーム終了
+  socket.on("timeIsUp", function() {
+    transMove($(".voteview"), $(".timeisup"), 0);
+    // データ送信
+    let inputData = {
+      "option" : $("input:radio[name='option']:checked").val(),
+      "amount" : $("#rBetVal").val()
+    }
+    //
+    socket.emit("postAnswer", inputData);
+  });
+
+  // 結果表示内容更新
+  socket.on("refreshSpan", function(isCorrect, curt, result, amount, magnif, cOption, cMagnif) {
+    if (isCorrect) {
+      $(".taResult").text(`おめでとう！`);
+      $(".tasGetScore").text(`${amount*magnif}P`);
+      $(".taBalloon").html(`+${amount*magnif}P`);
+    } else {
+      $(".taResult").text(`残念...`);
+      $(".tasGetScore").text(`-${amount}P`);
+      $(".taBalloon").html(`-${amount}P`);
+    }
+    $(".taTitle").text(`${cOption}`);
+    $(".taMagnif").text(`x${cMagnif}`);
+    $(".tasAmountVal").text(`${amount}P`);
+    $(".tasMagnifVal").text(`${magnif}`);
+    $(".tatScoreVal").text(`${result}P`);
+  });
+
+  // 順位更新
+  socket.on("refreshRank", function(rank, correctts) {
+    $(".taRank").text(`${correctts[1]}チーム中${rank}位`);
+    $(".taCateam").text(`${correctts[1]}チーム中${correctts[0]}チームが正解！`);
+  });
+
+  // 結果表示へ遷移
+  socket.on("resultView", function() {
+    transMove($(".timeisup"), $(".resultview"), 0);
+  });
+
+  // 最終結果表示へ遷移
+  socket.on("finrView", function(data) {
+    // html更新
+    // 表彰台
+    for (var i = 0; i < 3; i++) {
+      let parent = $(`#pod${i+1}`);
+      parent.find(".fpTeamname").html(data.rank[i][0]);
+      parent.find(".fpScore").html(`${data.rank[i][1]}P`);
+    }
+    // ペアチームのスコア
+    let parescore = 0;
+    let pareteam = ownteam.slice(0, -1) + "1";
+    if (ownteam.slice(-1) == "1") {
+      pareteam = ownteam.slice(0, -1) + "2";
+    }
+    // 自分の順位とスコア
+    for (var i = 0; i < data.rank.length; i++) {
+      // 自ちーむ
+      if (data.rank[i][0] == ownteam) {
+        $(".tRankBig").html(`${i+1}`);
+        $(".tScore").html(`${data.rank[i][1]}P`);
+        parescore += parseInt(data.rank[i][1]);
+      }
+      // ペアチーム
+      if (data.rank[i][0] == pareteam) {
+        $(".tpScoreInfo").html(`チーム${pareteam}のスコア`);
+        $(".tpScore").html(`${data.rank[i][1]}P`);
+        parescore += parseInt(data.rank[i][1]);
+      }
+    }
+    // 合計スコア表示
+    $(".ttScore").html(`${parescore}P`);
+    //
+    transMove($(".resultview"), $(".finalview"), 0);
+  });
+
+  // バグ回避のためcsv更新で強制ログアウト
+  socket.on("referCsvCli", function() {
+    location.reload();
+  });
+});
